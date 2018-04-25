ca544d2f522e7e9cb1854541f011d37e096cd5a3
Michael Bruns <michaelbruns99@gmail.com>
2018-04-24 22:25:45 -0400

    getting data from google api

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const request = require('request');
 const config = require('./config.json');
 
 request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=10185%20little%20forest%20drive%20germantown&key=${config.apiKey}`,
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=5115%20Lake%20Road&key=${config.apiKey}`,
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  if (response.statusCode === 200) {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`lat: ${body.results[0].geometry.location.lat}`);
+    console.log(`lng: ${body.results[0].geometry.location.lng}`);
+  }
+  // console.log(JSON.stringify(body, undefined, 2));
 });
