ef82fa11351a1377485ef40af0fbd3b0fb36147b
Michael Bruns <michaelbruns99@gmail.com>
2018-04-24 22:57:40 -0400

    added some error handling

--- app.js
+++ app.js
@@ -21,7 +21,11 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${config.apiKey}`,
   json: true
 }, (error, response, body) => {
-  if (response.statusCode === 200) {
+  if (error) {
+    console.log('Unable to connect to Google service');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Unable to find that address');
+  } else if (body.status === 'OK') {
     console.log(`Address: ${body.results[0].formatted_address}`);
     console.log(`lat: ${body.results[0].geometry.location.lat}`);
     console.log(`lng: ${body.results[0].geometry.location.lng}`);
