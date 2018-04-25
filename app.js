495d9f4e05af3229528f2c9f9c2415a9bbd6ce0b
Michael Bruns <michaelbruns99@gmail.com>
2018-04-25 00:05:27 -0400

    added forecast request to forecast.io

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const yargs = require('yargs');
-const geocode = require('./geocode/geocode.js')
+const geocode = require('./geocode/geocode.js');
+const config = require('./config.json');
 
 const argv = yargs
   .options({
@@ -19,5 +20,17 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
     console.log(errorMessage);
   } else {
     console.log(JSON.stringify(results, undefined, 2));
+  }
+});
+
+const request = require('request');
+request({
+  url: `https://api.darksky.net/forecast/${config.forecastApiKey}/41.4975517,-82.0808441`,
+  json: true
+}, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    console.log(body.currently.temperature);
+  } else {
+    console.log('Unable to fetch weather');
   }
 });
