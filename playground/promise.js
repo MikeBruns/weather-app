3846c6415088317137c85a09c4a2370ac2b0ab26
Michael Bruns <michaelbruns99@gmail.com>
2018-05-13 23:40:01 -0400

    mulitiple promises

--- playground/promise.js
+++ playground/promise.js
@@ -1,3 +1,37 @@
+var asyncAdd = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof a === 'number' && typeof b === 'number') {
+        resolve(a + b);
+      } else {
+        reject('Arguments must be nubmers');
+      }
+    }, 1500);
+  });
+};
+
+asyncAdd(1, 2)
+//resovle
+.then((result) => {
+  console.log('Result:', result);
+  return asyncAdd(result, 33);
+//reject
+}, (errorMessage) => {
+  console.log(errorMessage);
+})
+//Promise once the previous promise finishes
+//resolve
+.then((result) => {
+  console.log('Second promise:', result);
+//reject
+}, (errorMessage) => {
+  console.log(errorMessage);
+});
+
+
+///////////////////////////////////////////////////////////////
+
+
 var somePromise = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve('Hey. It worked!');
