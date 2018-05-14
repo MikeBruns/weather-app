dae7fc2f55b41a8c48b5c573b32e16ef2bec7217
Michael Bruns <michaelbruns99@gmail.com>
2018-05-13 20:50:06 -0400

    finishing up promise testing

--- playground/promise.js
+++ playground/promise.js
@@ -1,11 +1,11 @@
 var somePromise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    // resolve('Hey. It worked!');
-    reject('Unable to fulfill promise');
+    resolve('Hey. It worked!');
+    // reject('Unable to fulfill promise');
   }, 2500);
 });
 
-//only gets called if the promis is fulfilled, aka: no errors
+//only gets called if the promise is fulfilled, aka: no errors
 somePromise.then((message) => {
   console.log('Success: ', message);
 }, (errorMessage) => {
